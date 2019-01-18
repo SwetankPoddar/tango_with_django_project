117d41c874582f67a2a8695e13567e4a46e75151
Swetank Poddar <2424088p@student.gla.ac.uk>
2019-01-18 16:43:28 +0000

    chapter 4 - done

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,4 @@ def index(request):
 	context_dict = {'boldmessage':"Crunchy, creamy, cookie, candy, cupcake!"}
 	return render(request,'rango/index.html',context=context_dict)
 def about(request):
-	return HttpResponse("Rango says here is the about page. <br><a href='/rango/'>Index</a>")
+	return render(request,'rango/about.html')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.media',
             ],
         },
     },
@@ -122,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Media Files (dynamic file uploading)
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
