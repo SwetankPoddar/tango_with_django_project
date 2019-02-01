42ae8af4a7699992c5f4df13f9c706af4c4d95f7
Swetank Poddar <2424088p@student.gla.ac.uk>
2019-02-01 15:25:51 +0000

    Chapter 8.3 done without exercises

--- rango/forms.py
+++ rango/forms.py
@@ -19,6 +19,16 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
+
     class Meta:
         model = Page
         exclude = ('category',)
